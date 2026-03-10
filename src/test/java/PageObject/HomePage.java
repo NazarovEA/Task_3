485ae3124801aba006df4b2cf92c072c79ea8e40
Евгений <nk-nazarovEA@yandex.ru>
2026-03-11 00:32:54 +0300

    тест перехода из личного кабинета в логотип

--- src/test/java/PageObject/HomePage.java
+++ src/test/java/PageObject/HomePage.java
@@ -2,6 +2,10 @@ package PageObject;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class HomePage {
     private WebDriver driver;
@@ -9,7 +13,8 @@ public class HomePage {
     private final By loginButtonMain = By.xpath(".//button[text()='Войти в аккаунт']");
     // Кнопка "Личный кабинет"
     private final By personalAccountButton = By.xpath(".//p[text()='Личный Кабинет']");
-
+// логотип
+private final By logo = By.xpath(".//header//a[@href='/']");
 
     public HomePage(WebDriver driver) {
         this.driver = driver;
@@ -22,4 +27,11 @@ public class HomePage {
     public void clickPersonalAccountButton() {
         driver.findElement(personalAccountButton).click();
     }
+
+    public void clickLogo() {
+        new WebDriverWait(driver, Duration.ofSeconds(10))
+                .until(ExpectedConditions.elementToBeClickable(logo))
+                .click();
+    }
+
 }
--- src/test/java/ProfileTest.java
+++ src/test/java/ProfileTest.java
@@ -20,7 +20,7 @@ public class ProfileTest {
 
     @ParameterizedTest
     @ValueSource(strings = {"yandex"})
-    public void loginLogYourAccount(String browser) {
+    public void inConstructor(String browser) {
         WebDriverFactory factory = new WebDriverFactory();
         driver = factory.getWebDriver(browser);
         driver.get("https://stellarburgers.education-services.ru");
@@ -37,6 +37,30 @@ public class ProfileTest {
 
         new HomePage(driver).clickPersonalAccountButton();
         new ProfilePage(driver).clickConstructorButton();
+
+        new WebDriverWait(driver, Duration.ofSeconds(10))
+                .until(ExpectedConditions.visibilityOfElementLocated(By.xpath(".//button[text()='Оформить заказ']")));
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"yandex"})
+    public void inLogo(String browser) {
+        WebDriverFactory factory = new WebDriverFactory();
+        driver = factory.getWebDriver(browser);
+        driver.get("https://stellarburgers.education-services.ru");
+
+// После открытия главной страницы, заходим и вносим имя и пароль
+        new HomePage(driver).clickLoginButton();
+
+        LoginPage loginPage = new LoginPage(driver);
+        new WebDriverWait(driver, Duration.ofSeconds(10))
+                .until(ExpectedConditions.visibilityOfElementLocated(By.name("name")));
+        // Переходим на логин
+
+        loginPage.login("evgen88@ya.ru", "123456"); // Логинимся
+
+        new HomePage(driver).clickPersonalAccountButton();
+        new HomePage(driver).clickLogo();
 
         new WebDriverWait(driver, Duration.ofSeconds(10))
                 .until(ExpectedConditions.visibilityOfElementLocated(By.xpath(".//button[text()='Оформить заказ']")));
