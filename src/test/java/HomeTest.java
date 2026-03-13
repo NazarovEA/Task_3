3d00c1ea373f0267c5c70254a09b73a0f78dec06
Евгений <nk-nazarovEA@yandex.ru>
2026-03-14 02:07:31 +0300

    добавил закрытие браузера

---
+++ src/test/java/HomeTest.java
@@ -0,0 +1,111 @@
+import PageObject.HomePage;
+import PageObject.LoginPage;
+import org.example.WebDriverFactory;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class HomeTest {
+    WebDriver driver;
+
+    @ParameterizedTest
+    @ValueSource(strings = {"chrome"})
+    public void fillingsOpen(String browser) {
+        WebDriverFactory factory = new WebDriverFactory();
+        driver = factory.getWebDriver(browser);
+        driver.get("https://stellarburgers.education-services.ru");
+
+// После открытия главной страницы, заходим и вносим имя и пароль
+        new HomePage(driver).clickLoginButton();
+
+        LoginPage loginPage = new LoginPage(driver);
+        new WebDriverWait(driver, Duration.ofSeconds(10))
+                .until(ExpectedConditions.visibilityOfElementLocated(By.name("name")));
+        // Переходим на логин
+
+        loginPage.login("evgen881@ya.ru", "NazarovYandex355"); // Логинимся
+        //добавляем ожидание, так как не успевает страница открыться
+        new WebDriverWait(driver, Duration.ofSeconds(10))
+                .until(ExpectedConditions.visibilityOfElementLocated(By.xpath(".//button[text()='Оформить заказ']")));
+
+       //new HomePage(driver).clickBuns();
+      // new HomePage(driver).clickSauces();
+       new HomePage(driver).clickFillings();
+        HomePage homePage = new HomePage(driver);
+        assertTrue(homePage.fillingsTabActive(), "Вкладка 'Начинки' не стала активной после клика!");
+
+    }
+    @AfterEach
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }}
+
+    @ParameterizedTest
+    @ValueSource(strings = {"chrome"})
+    public void sausesOpen(String browser) {
+        WebDriverFactory factory = new WebDriverFactory();
+        driver = factory.getWebDriver(browser);
+        driver.get("https://stellarburgers.education-services.ru");
+
+// После открытия главной страницы, заходим и вносим имя и пароль
+        new HomePage(driver).clickLoginButton();
+
+        LoginPage loginPage = new LoginPage(driver);
+        new WebDriverWait(driver, Duration.ofSeconds(10))
+                .until(ExpectedConditions.visibilityOfElementLocated(By.name("name")));
+        // Переходим на логин
+
+        loginPage.login("evgen881@ya.ru", "NazarovYandex355"); // Логинимся
+        //добавляем ожидание, так как не успевает страница открыться
+        new WebDriverWait(driver, Duration.ofSeconds(10))
+                .until(ExpectedConditions.visibilityOfElementLocated(By.xpath(".//button[text()='Оформить заказ']")));
+
+        //new HomePage(driver).clickBuns();
+        new HomePage(driver).clickSauces();
+        HomePage homePage = new HomePage(driver);
+        assertTrue(homePage.saucesTabActive(), "Вкладка 'Начинки' не стала активной после клика!");
+
+    }
+    @AfterEach
+    public void tearDown2() {
+        if (driver != null) {
+            driver.quit();
+        }}
+
+    @ParameterizedTest
+    @ValueSource(strings = {"chrome"})
+    public void bunsOpen(String browser) {
+        WebDriverFactory factory = new WebDriverFactory();
+        driver = factory.getWebDriver(browser);
+        driver.get("https://stellarburgers.education-services.ru");
+
+// После открытия главной страницы, заходим и вносим имя и пароль
+        new HomePage(driver).clickLoginButton();
+
+        LoginPage loginPage = new LoginPage(driver);
+        new WebDriverWait(driver, Duration.ofSeconds(10))
+                .until(ExpectedConditions.visibilityOfElementLocated(By.name("name")));
+        // Переходим на логин
+
+        loginPage.login("evgen881@ya.ru", "NazarovYandex355"); // Логинимся
+        //добавляем ожидание, так как не успевает страница открыться
+        new WebDriverWait(driver, Duration.ofSeconds(10))
+                .until(ExpectedConditions.visibilityOfElementLocated(By.xpath(".//button[text()='Оформить заказ']")));
+
+
+        new HomePage(driver).clickBuns();
+        HomePage homePage = new HomePage(driver);
+        assertTrue(homePage.bunsTabActive(), "Вкладка 'Начинки' не стала активной после клика!");
+    }
+
+
+}
