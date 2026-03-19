5f7b7ff49f056a72f2b3bf79c83bc8cd9650faa4
Евгений <nk-nazarovEA@yandex.ru>
2026-03-20 01:39:02 +0300

    HomeTest подредактировал

--- src/test/java/HomeTest.java
+++ src/test/java/HomeTest.java
@@ -1,3 +1,4 @@
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import pageobject.HomePage;
@@ -10,59 +11,28 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class HomeTest {
     WebDriver driver;
+    HomePage homePage;
+
+    @BeforeEach
+    public void setUp() {
+        String browser = System.getProperty("browser", "chrome");
+        WebDriverFactory factory = new WebDriverFactory();
+        driver = factory.getWebDriver(browser);
+        driver.get(HomePage.BASE_URL);
+        homePage = new HomePage(driver);
+    }
 
     @Test
     @DisplayName("Переход к начинкам")
     public void fillingsOpen() {
-        String browser = System.getProperty("browser", "chrome");
-        WebDriverFactory factory = new WebDriverFactory();
-        driver = factory.getWebDriver(browser);
-        driver.get(HomePage.BASE_URL);
-
-// После открытия главной страницы, заходим и вносим имя и пароль
-        new HomePage(driver).clickLoginButton();
-        HomePage homePage = new HomePage(driver);
-        LoginPage loginPage = new LoginPage(driver);
-        loginPage.waitForLoad();
-        // Переходим на логин
-
-        loginPage.login("evgen881@ya.ru", "NazarovYandex355"); // Логинимся
-        //добавляем ожидание, так как не успевает страница открыться
-        homePage.waitForOrderButton();
-
-       //new HomePage(driver).clickBuns();
-      // new HomePage(driver).clickSauces();
-       new HomePage(driver).clickFillings();
+        homePage.clickFillings();
         assertTrue(homePage.fillingsTabActive(), "Вкладка 'Начинки' не стала активной после клика!");
-
     }
-    @AfterEach
-    public void tearDown() {
-        if (driver != null) {
-            driver.quit();
-        }}
 
     @Test
     @DisplayName("Переход к соусам")
     public void sausesOpen() {
-        String browser = System.getProperty("browser", "chrome");
-        WebDriverFactory factory = new WebDriverFactory();
-        driver = factory.getWebDriver(browser);
-        driver.get(HomePage.BASE_URL);
-
-// После открытия главной страницы, заходим и вносим имя и пароль
-        new HomePage(driver).clickLoginButton();
-        HomePage homePage = new HomePage(driver);
-        LoginPage loginPage = new LoginPage(driver);
-        loginPage.waitForLoad();
-        // Переходим на логин
-
-        loginPage.login("evgen881@ya.ru", "NazarovYandex355"); // Логинимся
-        //добавляем ожидание, так как не успевает страница открыться
-        homePage.waitForOrderButton();
-
-        //new HomePage(driver).clickBuns();
-        new HomePage(driver).clickSauces();
+        homePage.clickSauces();
         assertTrue(homePage.saucesTabActive(), "Вкладка 'соусы' не стала активной после клика!");
 
     }
@@ -70,26 +40,14 @@ public class HomeTest {
     @Test
     @DisplayName("Переход к булкам")
     public void bunsOpen() {
-        String browser = System.getProperty("browser", "chrome");
-        WebDriverFactory factory = new WebDriverFactory();
-        driver = factory.getWebDriver(browser);
-        driver.get(HomePage.BASE_URL);
-
-// После открытия главной страницы, заходим и вносим имя и пароль
-        new HomePage(driver).clickLoginButton();
-        HomePage homePage = new HomePage(driver);
-        LoginPage loginPage = new LoginPage(driver);
-        loginPage.waitForLoad();
-        // Переходим на логин
-
-        loginPage.login("evgen881@ya.ru", "NazarovYandex355"); // Логинимся
-        //добавляем ожидание, так как не успевает страница открыться
-        homePage.waitForOrderButton();
-
-
-        new HomePage(driver).clickBuns();
+        homePage.clickSauces();
+        homePage.clickBuns();
         assertTrue(homePage.bunsTabActive(), "Вкладка 'булки' не стала активной после клика!");
     }
 
-
+     @AfterEach
+     public void tearDown() {
+         if (driver != null) {
+            driver.quit();
+        }}
 }
